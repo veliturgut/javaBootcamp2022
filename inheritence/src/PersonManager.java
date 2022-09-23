48b21b70379a3e7601d9658a5ec507f9a91cc916
veliturgut <turgut.v16@gmail.com>
2022-09-23 20:33:03 +0300

    work2

---
+++ inheritence/src/PersonManager.java
@@ -0,0 +1,10 @@
+public class PersonManager {
+
+    public void List(){
+        System.out.println("listelendi");
+    }
+
+    public void Add(){
+        System.out.println("Eklendi");
+    }
+}
