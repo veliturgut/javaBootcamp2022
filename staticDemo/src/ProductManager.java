3c8369287bd385ef626987f4c620ba2c7cf64576
veliturgut <turgut.v16@gmail.com>
2022-09-27 16:53:24 +0300

    Static class test

---
+++ staticDemo/src/ProductManager.java
@@ -0,0 +1,15 @@
+import javax.xml.bind.ValidationEvent;
+
+public class ProductManager {
+
+    public void add(Product product){
+
+        ProductValidator validator = new ProductValidator();
+        if (validator.isValid(product)){
+            System.out.println("Eklendi");
+        }else {
+            System.out.println("Ürün bilgileri geçersizdir.");
+        }
+
+    }
+}
