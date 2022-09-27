417f43d30bdd5fbe0505f134e7791b82b37e3f94
veliturgut <turgut.v16@gmail.com>
2022-09-27 16:12:26 +0300

    interface

---
+++ interfaces/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
+
+        customerManager.add();
+
+
+    }
+}
